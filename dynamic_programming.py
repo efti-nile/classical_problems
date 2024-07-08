802670ee75d66b13ff4548f5852ab58ff8b5194d
Efti Nile <efti.nile@gmail.com>
2024-07-08 17:51:47 +0400

    add knapsack

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -19,7 +19,39 @@ def lcs(X, Y):
     return dp[m][n]
 
 
-if __name__ == '__main__':
+def test_lcs():
+    print("Longest Commons Subsequence:")
     X = "AGGTAB"
-    Y = "GXTXAYB"
+    Y = "AGXTXAYB"
     print(lcs(X, Y))
+
+
+def knapsack(weights, values, capacity):
+    """Knapsack Problem"""
+
+    n = len(weights)
+    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+    
+    for i in range(1, n + 1):
+        for w in range(1, capacity + 1):
+            if weights[i - 1] <= w:
+                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1])
+            else:
+                dp[i][w] = dp[i - 1][w]
+
+    pprint.pp(dp)
+                
+    return dp[n][capacity]
+
+
+def test_knapsack():
+    print("Knapsack:")
+    weights = [1, 2, 3, 4]
+    values = [1, 4, 4, 5]
+    capacity = 5
+    print(knapsack(weights, values, capacity))
+
+
+if __name__ == '__main__':
+    test_lcs()
+    test_knapsack()
